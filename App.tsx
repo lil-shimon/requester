89a07f9bc54afbd1a8d0d6b8a974efea5149142e
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-01-09 23:51:47 +0900

    feat: 🎸 main screen

--- App.tsx
+++ App.tsx
@@ -1,24 +1,10 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import MainScreen from './src/screens/main'
 import AppContainer from './src/components/app-container'
 
 export default function App() {
   return (
     <AppContainer>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <Text>Hello world!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <MainScreen />
     </AppContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
